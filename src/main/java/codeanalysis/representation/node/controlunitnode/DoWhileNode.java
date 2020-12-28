f9db6ea552951594141c39b0c9923d2bf2519bf3
codeplay98 <1290526355@qq.com>
2020-12-28 19:56:17 +0800

    Constuct Control Flow

---
+++ src/main/java/codeanalysis/representation/node/controlunitnode/DoWhileNode.java
@@ -0,0 +1,9 @@
+package codeanalysis.representation.node.controlunitnode;
+
+import codeanalysis.representation.GraphNode;
+
+/**
+ * @author coldilock
+ */
+public class DoWhileNode extends GraphNode {
+}
